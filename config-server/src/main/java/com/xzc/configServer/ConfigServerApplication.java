9c27366c8edfae3fa16f632aa946af948e62e214
SimonShaw25 <961197998@qq.com>
2019-03-28 21:42:11 +0800

    第一次提交

---
+++ config-server/src/main/java/com/xzc/configServer/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.xzc.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
+
+}
