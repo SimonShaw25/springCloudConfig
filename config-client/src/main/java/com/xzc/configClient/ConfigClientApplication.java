3039ef0a86f687e90280fd2300e80c74dbd25965
SimonShaw25 <961197998@qq.com>
2019-03-31 21:13:30 +0800

    提交配置文件

--- config-client/src/main/java/com/xzc/configClient/ConfigClientApplication.java
+++ config-client/src/main/java/com/xzc/configClient/ConfigClientApplication.java
@@ -3,17 +3,20 @@ package com.xzc.configClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ConfigClientApplication
 {
 
 
   @Value("${foo}")
   String foo;
+
   public static void main(String[] args)
   {
     SpringApplication.run(ConfigClientApplication.class, args);
